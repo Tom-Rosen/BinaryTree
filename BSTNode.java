41796506854a46a91defaa9ff7dafc66fe4bd0f7
Tom Rosen <107373056+Tom-Rosen@users.noreply.github.com>
2022-06-19 17:43:12 -0500

    Update BSTNode.java

--- BSTNode.java
+++ BSTNode.java
@@ -1,9 +1,11 @@
-// DO NOT SUBMIT THIS SOURCE FILE TO GRADESCOPE
+// Name: Tom Rosen
+// Email: trrosen @wisc.edu 
+
 /**
  * Generic class implementing a Binary Node of a Binary Search Tree (BST)
  *
  * @param <T> type of the data carried by this binary node
- * @author Sulong
+ * 
  */
 public class BSTNode<T extends Comparable<T>>{
   // Note: DO NOT MAKE ANY CHANGE TO THIS CLASS AND DO NOT SUBMIT IT TO GRADESCOPE
